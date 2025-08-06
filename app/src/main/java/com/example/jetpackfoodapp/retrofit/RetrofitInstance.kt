b6515851045dde13ee166e919622a32bdbb6447d
Chaitnya-Ghai <chaitnyaghai2004@gmail.com>
2025-08-06 08:37:29 +0530

    Enhance Retrofit with OkHttp logging and timeouts
    - Added `HttpLoggingInterceptor` to log request and response bodies.
    - Configured read, write, and connect timeouts to 30 seconds for the `OkHttpClient`.

--- app/src/main/java/com/example/jetpackfoodapp/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/jetpackfoodapp/retrofit/RetrofitInstance.kt
@@ -1,13 +1,24 @@
 package com.example.jetpackfoodapp.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     val api:CategoryApi by lazy {
+        val client = OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        client.addInterceptor(logging)
         Retrofit.Builder()
             .baseUrl("https://www.themealdb.com/api/json/v1/1/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client.build())
             .build()
             .create(CategoryApi::class.java)
     }
